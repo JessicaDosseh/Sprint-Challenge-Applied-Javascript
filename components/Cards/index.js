3a9a40caeba050c8e5e0675c9738c755703978ec
Jessica <Jessica@users-iMac.local>
2019-11-09 13:58:57 -0500

    Header, Tabs, Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,81 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+// Data Array
+//let articles = []; 
+
+// // Call HTML component block
+// const cardsContainer = document.querySelector(".cards-container"); 
+
+// AXIOS 1: .get(), 2: .then(), 3. .catch()
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    // Call HTML component block
+    const cardsContainer = document.querySelector(".cards-container");
+    console.log(response); 
+
+    response.data.articles.bootstrap.forEach(element => {
+      cardsContainer.appendChild(cardCreator(element)); 
+    })
+
+    response.data.articles.javascript.forEach(element => {
+      cardsContainer.appendChild(cardCreator(element)); 
+    })
+
+    response.data.articles.technology.forEach(element => {
+      cardsContainer.appendChild(cardCreator(element)); 
+    })
+
+    response.data.articles.jquery.forEach(element => {
+      cardsContainer.appendChild(cardCreator(element)); 
+    })
+
+    response.data.articles.node.forEach(element => {
+      cardsContainer.appendChild(cardCreator(element)); 
+    })
+
+  })
+  .catch(error => {
+    console.log("Error: ", error); 
+  })
+
+  console.log(articles); 
+
+  //Component Function 
+  function articleCreator(data) {
+
+    // Step 1 Create Element tags
+    const article = document.createElement("div"); 
+    const header = document.createElement("div"); 
+    const writer = document.createElement("div"); 
+    const imgBox = document.createElement("div"); 
+    const img = document.createElement("img"); 
+    const writerName = document.createElement("span"); 
+
+    // Step 2 Place components into HTML page
+    //cardsContainer.appendChild(article); 
+
+    article.appendChild(header); 
+    article.appendChild(writer); 
+
+    writer.appendChild(imgBox); 
+    writer.appendChild(writerName); 
+
+    imgBox.appendChild(img); 
+
+    // Step 3 Add classes to each tag
+    article.classList("card"); 
+    header.classList("headline"); 
+    writer.classList("author"); 
+    imgBox.classList("img-container");
+    
+    // Step 4 Fill each tag with the apropriate content
+    header.textContent = data.headline; 
+    img.src = data.authorPhoto; 
+    writerName.textContent = `By ${data.authorName}`; 
+
+    return article; 
+  }
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,40 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+// Data Array
+let topics = []; 
+
+// Call HTML component block
+const tabs = document.querySelector(".topics"); 
+
+// AXIOS 1: .get(), 2: .then(), 3. .catch()
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response); 
+
+    response.data.topics.forEach(element => {
+      topics.push(element); 
+    })
+    
+    topics.forEach(element => {
+      tabs.appendChild(tabCreator(element)); 
+    })
+  })
+  .catch(error => {
+    console.log("Error: ", error); 
+  })
+
+  console.log(topics);  
+
+  //Component Function
+  function tabCreator(element) {
+    // Step 1-3 Create Element, Add Class, Add Content
+    const tab = document.createElement("div"); 
+    tab.classList.add("tab"); 
+    tab.textContent = element; 
+
+    return tab; 
+  }
